176837c7dd21446667772055535c6f7c5aa07eee
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2022-08-22 17:35:34 +0700

    add techstack to project

--- src/components/Portofolio.js
+++ src/components/Portofolio.js
@@ -3,13 +3,52 @@ import TLM from '../assets/img/4.png';
 import JKT from '../assets/img/3.png';
 import DLR from '../assets/img/2.png';
 import SMT from '../assets/img/1.png';
-import { FaLaptopCode , FaReact, FaHtml5} from 'react-icons/fa';
+import { FaLaptopCode , FaReact, FaHtml5, FaLaravel, FaPhp } from 'react-icons/fa';
 import { TbZoomCode } from 'react-icons/tb';
 import { AiFillGithub } from 'react-icons/ai';
 import { BsFillBootstrapFill } from 'react-icons/bs';
 import { IoLogoNodejs } from 'react-icons/io';
+import { SiMysql } from 'react-icons/si';
 
 function Portofolio() {
+
+  const TechStack = () => (
+    <div>
+      <div className="flex justify-center py-3 space-x-3">
+        <a className=' text-white bg-sky-500 py-2 px-3 rounded-xl hover:shadow-lg hover:opacity-70 transition duration-200 ease-in-out' target="_blank">
+          <FaReact className='mb-1 inline' size={20} />
+        </a>
+        <a className=' text-white bg-indigo-700 py-2 px-3 rounded-xl hover:shadow-lg hover:opacity-70 transition duration-200 ease-in-out' target="_blank">
+          <BsFillBootstrapFill className='mb-1 inline' size={20} />
+        </a>
+        <a className=' text-white bg-[#e34c26] py-2 px-3 rounded-xl hover:shadow-lg hover:opacity-70 transition duration-200 ease-in-out' target="_blank">
+          <FaHtml5 className='mb-1 inline' size={20} />
+        </a>
+        <a className=' text-white bg-[#6cc24a] py-2 px-3 rounded-xl hover:shadow-lg hover:opacity-70 transition duration-200 ease-in-out' target="_blank">
+          <IoLogoNodejs className='mb-1 inline' size={20} />
+        </a>
+      </div>
+    </div>
+  )
+
+  const TechStackPHP = () => (
+    <div>
+      <div className="flex justify-center py-3 space-x-3">
+        <a className=' text-white bg-red-500 py-2 px-3 rounded-xl hover:shadow-lg hover:opacity-70 transition duration-200 ease-in-out' target="_blank">
+          <FaLaravel className='mb-1 inline' size={20} />
+        </a>
+        <a className=' text-white bg-indigo-700 py-2 px-3 rounded-xl hover:shadow-lg hover:opacity-70 transition duration-200 ease-in-out' target="_blank">
+          <BsFillBootstrapFill className='mb-1 inline' size={20} />
+        </a>
+        <a className=' text-white bg-[#474A8A] py-2 px-3 rounded-xl hover:shadow-lg hover:opacity-70 transition duration-200 ease-in-out' target="_blank">
+          <FaPhp className='mb-1 inline' size={20} />
+        </a>
+        <a className=' text-white bg-[#00758f] py-2 px-3 rounded-xl hover:shadow-lg hover:opacity-70 transition duration-200 ease-in-out' target="_blank">
+          <SiMysql className='mb-1 inline' size={20} />
+        </a>
+      </div>
+    </div>
+  )
 
   const projects = [
     {
@@ -17,45 +56,31 @@ function Portofolio() {
       title: 'The Lazy Media',
       desc: 'The Lazy Media is your games, technology, entertainment, and gadgets website. I remake this website for reading some article news with diffrent UI / UX',
       link: 'https://the-lazy-media.vercel.app/',
-      github: 'https://github.com/ikhbaldwiyan/the-lazy-media'
+      github: 'https://github.com/ikhbaldwiyan/the-lazy-media',
+      technology: <TechStack />
     },
     {
       img: JKT,
       title: 'JKT 48 SHOWROOM',
       desc: 'Showroom is one of the largest live streaming platforms in Japan! this website is filtered streaming for JKT48 room only based by API that i made',
       link: 'https://www.jkt48-showroom.com/',
-      github: 'https://github.com/ikhbaldwiyan/showroom'
+      github: 'https://github.com/ikhbaldwiyan/showroom',
+      technology: <TechStack />
     },
     {
       img: SMT,
       title: 'Smooth Pizza',
-      desc: 'Website Point of sale for management pizza restaurant, This website made at my high school last project'
+      desc: 'Website Point of sale for management pizza restaurant, This website made at my high school last project',
+      technology: <TechStackPHP />
+
     },
     {
       img: DLR,
       title: 'Jack Dealer',
-      desc: 'Website dealer for buy or sell cars, you can buy many type of branding cars in this website'
+      desc: 'Website dealer for buy or sell cars, you can buy many type of branding cars in this website',
+      technology: <TechStackPHP />
     },
-  ]
-
-  const TechStack = () => (
-    <div>
-      <div className="flex justify-center py-3 space-x-3">
-        <a className='text-base font-semibold text-white bg-sky-500 py-2 px-3 rounded-xl hover:shadow-lg hover:opacity-70 transition duration-200 ease-in-out' target="_blank">
-          <FaReact className='mb-1 inline' size={20} />
-        </a>
-        <a className='text-base font-semibold text-white bg-indigo-700 py-2 px-3 rounded-xl hover:shadow-lg hover:opacity-70 transition duration-200 ease-in-out' target="_blank">
-          <BsFillBootstrapFill className='mb-1 inline' size={20} />
-        </a>
-        <a className='text-base font-semibold text-white bg-[#e34c26] py-2 px-3 rounded-xl hover:shadow-lg hover:opacity-70 transition duration-200 ease-in-out' target="_blank">
-          <FaHtml5 className='mb-1 inline' size={20} />
-        </a>
-        <a className='text-base font-semibold text-white bg-[#6cc24a] py-2 px-3 rounded-xl hover:shadow-lg hover:opacity-70 transition duration-200 ease-in-out' target="_blank">
-          <IoLogoNodejs className='mb-1 inline' size={20} />
-        </a>
-      </div>
-    </div>
-  )
+  ]  
 
   return (
     <section id='project' className='pt-22 pb-16 bg-slate-100 rounded-lg my-10 mx-4'>
@@ -81,9 +106,9 @@ function Portofolio() {
               <h3 className="font-semibold text-xl text-black py-3 uppercase">
                 {item.title}
               </h3>
-              {item.link && (
-                <TechStack />
-              )}
+
+              {item.technology}
+
               <p className="text-secondary font-medium mb-10">
                 {item.desc}
               </p>
@@ -94,7 +119,6 @@ function Portofolio() {
               <a href={item.github} className={`text-base font-semibold text-white bg-black py-4 px-4 rounded-xl hover:shadow-lg hover:opacity-70 transition duration-200 ease-in-out inline ${!item.link ? 'opacity-60 cursor-not-allowed' : ''}`} target="_blank">
                 <AiFillGithub className='mb-1 inline mr-1' size={25} /> GitHub
               </a>
-            
             </div>
           ))}
         </div>
